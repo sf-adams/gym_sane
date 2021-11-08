c9d00a47db18d7656e1fd9baa3ca3dd235215847
Sam <samuel.fitz@btinternet.com>
2021-11-08 14:05:45 +0000

    Attendance Tests Added
    Importing the session and member classes, these were then used to create an instance of an attendance. Ran two tests, one regarding the member object and the other the session object, both passed.

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from tests.member_test import TestMember
 from tests.session_test import TestSession
+from tests.attendance_test import TestAttendance
 
 if __name__ == '__main__':
     unittest.main()
--- tests/attendance_test.py
+++ tests/attendance_test.py
@@ -1,8 +1,20 @@
 import unittest
 
 import unittest
+from models.member import Member
+from models.session import Session
 from models.attendance import Attendance
 
 class TestAttendance(unittest.TestCase):
     def setUp(self):
-        self.attendance = Attendance("")
+        self.member = Member("Sam", "Adams", 24, "Strength")
+        self.session = Session("No Pain No Gains", "07:00", "Strength")
+        self.attendance = Attendance(self.member, self.session)
+
+    def test_attendance_can_have_member(self):
+        self.assertEqual(self.member, self.attendance.member)
+    
+    def test_attendance_can_have_session(self):
+        self.assertEqual(self.session, self.attendance.session)
+    
+    
