d5691ef39b4711e4f0a6910bb0e4098d6c076f1c
Sam <samuel.fitz@btinternet.com>
2021-11-11 16:15:21 +0000

    New Bookings Can Now Be Added
    A member and an available booking are now selected from the dropdown menu, then the user can click submit and they are taken back to the main bookings page.

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -3,10 +3,8 @@ from flask import Blueprint, render_template, request, redirect
 from models.booking import Booking
 import repositories.booking_repository as booking_repository
 
-from models.session import Session
 import repositories.session_repository as session_repository
 
-from models.member import Member
 import repositories.member_repository as member_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
@@ -21,9 +19,11 @@ def bookings():
 # SHOW
 @bookings_blueprint.route("/bookings/<id>", methods=['GET'])
 def show(id):
-    member = booking_repository.select(id)
-    sessions = booking_repository.sessions(member)
-    return render_template("bookings/show.html", member=member, sessions=sessions)
+    member_id = request.form["member_id"]
+    session_id = request.form["session_id"]
+    member = member_repository.select(member_id)
+    session = session_repository.select(session_id)
+    return render_template("bookings/show.html", member=member, session=session)
 
 # NEW
 @bookings_blueprint.route("/bookings/new")
@@ -31,7 +31,7 @@ def new_booking():
     title = "New Booking"
     members = member_repository.select_all()
     sessions = session_repository.select_all()
-    return render_template("bookings/new.html", title=title, members=members)
+    return render_template("bookings/new.html", title=title, members=members, sessions=sessions)
 
 # CREATE
 @bookings_blueprint.route("/bookings", methods=["POST"])
