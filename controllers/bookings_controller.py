0987e6818622db353013f4c97d7e0aff7248a3a7
Sam <samuel.fitz@btinternet.com>
2021-11-11 10:52:51 +0000

    Bookings Section Added
    All bookings can now be viewed in the booking tab, but there is still a slight issue with adding a member to the booking.

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -17,3 +17,47 @@ def bookings():
     title = "Bookings"
     bookings = booking_repository.select_all()
     return render_template("bookings/index.html", title=title, bookings=bookings)
+
+# # SHOW
+# @bookings_blueprint.route("/bookings/<id>", methods=['GET'])
+# def show(id):
+#     member = booking_repository.select(id)
+#     sessions = booking_repository.sessions(member)
+#     return render_template("bookings/show.html", member=member, sessions=sessions)
+
+# NEW
+@bookings_blueprint.route("/bookings/new")
+def new_booking():
+    title = "New Booking"
+    return render_template("bookings/new.html", title=title)
+
+# CREATE
+@bookings_blueprint.route("/bookings", methods=["POST"])
+def create_booking():
+    member = request.form["member"]
+    session = request.form["session"]
+    new_booking = Booking(member, session)
+    booking_repository.save(new_booking)
+    return redirect("/bookings")
+
+# EDIT
+@bookings_blueprint.route("/bookings/<id>/edit", methods=['GET'])
+def edit_booking(id):
+    title = "Edit Booking"
+    booking = booking_repository.select(id)
+    return render_template("/bookings/edit.html", title=title, booking=booking)
+
+# UPDATE
+@bookings_blueprint.route("/bookings/<id>", methods=["POST"])
+def update_booking(id):
+    member = request.form["member"]
+    session = request.form["session"]
+    new_booking = Booking(member, session)
+    booking_repository.update(new_booking)
+    return redirect("/bookings")
+
+# DELETE
+@bookings_blueprint.route("/bookings/<id>/delete", methods=['POST'])
+def delete_booking(id):
+    booking_repository.delete(id)
+    return redirect("/bookings")
